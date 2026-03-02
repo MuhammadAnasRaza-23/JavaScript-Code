193fffb6cde82206d613d227c673819017db8d8e
Muhammad Anas Raza <anasraza2k6@gmail.com>
2026-03-02 15:26:07 +0500

    JS code
    practise JS code

---
+++ trial code/app.js
@@ -0,0 +1,75 @@
+// name of the user
+var firstname = prompt("Enter your first name");
+var middlename = prompt("Enter your middle name");
+var lastname = prompt("Enter your last name");
+var fullname = firstname + " " + middlename + " " + lastname;
+
+
+// user personal details
+var age = prompt("Enter your age");
+var address = prompt("Enter your address");
+var phone = prompt("Enter your phone number");
+var email = prompt("Enter your email");
+
+
+// user's favorite things
+var favoritecolor = prompt("Enter your favorite color");
+var favoritefood = prompt("Enter your favorite food");
+var favoritemovie = prompt("Enter your favorite movie");
+var favoritebook = prompt("Enter your favorite book");
+
+
+// user's hobbies
+var hobby1 = prompt("Enter your first hobby");
+var hobby2 = prompt("Enter your second hobby");
+var hobby3 = prompt("Enter your third hobby");
+
+
+// user's skills
+var skill1 = prompt("Enter your first skill");
+var skill2 = prompt("Enter your second skill");
+var skill3 = prompt("Enter your third skill");
+
+
+// user's goals
+var goal1 = prompt("Enter your first goal");
+var goal2 = prompt("Enter your second goal");
+var goal3 = prompt("Enter your third goal");
+
+
+// user's achievements
+var achievement1 = prompt("Enter your first achievement");
+var achievement2 = prompt("Enter your second achievement");
+var achievement3 = prompt("Enter your third achievement");
+
+
+// user's future plans
+var futureplan1 = prompt("Enter your first future plan");
+var futureplan2 = prompt("Enter your second future plan");
+var futureplan3 = prompt("Enter your third future plan");
+
+
+// user's favorite quote
+var favoritequote = prompt("Enter your favorite quote");
+
+// user's favorite song
+var favoritesong = prompt("Enter your favorite song");
+
+// user's favorite place    
+var favoriteplace = prompt("Enter your favorite place");
+
+// user's favorite memory
+var favoritememory = prompt("Enter your favorite memory");
+
+// user's favorite person
+var favoriteperson = prompt("Enter your favorite person");
+
+// user's favorite animal
+var favoriteanimal = prompt("Enter your favorite animal");
+alert( "Your full name is " + fullname + "\n" + "Your personal details are: " + "\n" + "Age: " + age + "\n" + "Address: " + address + "\n" + "Phone: " + phone + "\n" + "Email: " + email + "\n" + "Your favorite things are: " + "\n" + "Color: " + favoritecolor + "\n" + "Food: " + favoritefood + "\n" + "Movie: " + favoritemovie + "\n" + "Book: " + favoritebook  + "\n" +"Your hobbies are: "  + "\n" +"1. "+ hobby1  + "\n" +"2. "+ hobby2  + "\n" +"3. "+ hobby3 + "\n" + "Your skills are: " + "\n" + "1. " + skill1 + "\n" + "2. " + skill2 + "\n" + "3. " + skill3  + "\n" +"Your goals are: "+ "\n" +"1. "+ goal1  + "\n" +"2. "+ goal2  + "\n" +"3. "+ goal3  + "\n" +"Your achievements are: "+ "\n" +"1. "+ achievement1  + "\n" +"2. "+ achievement2  + "\n" +"3. "+ achievement3  + "\n" +"Your future plans are: "+ "\n" +"1. "+ futureplan1  + "\n" +"2. "+ futureplan2  + "\n" +"3. "+ futureplan3  + "\n" +"Your favorite quote is: " + favoritequote  + "\n" +"Your favorite song is: " + favoritesong  + "\n" +"Your favorite place is: " + favoriteplace  + "\n" +"Your favorite memory is: " + favoritememory  + "\n" +"Your favorite person is: " + favoriteperson  + "\n" +"Your favorite animal is: " + favoriteanimal)
+
+
+
+
+
+ 
