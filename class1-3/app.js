296708eaca51d537d3c0e50d52a7eb762bd23b31
Muhammad Anas Raza <anasraza2k6@gmail.com>
2026-03-02 15:25:24 +0500

    js code
    class 1-3

---
+++ class1-3/app.js
@@ -0,0 +1,67 @@
+// pre inc = +1 before carrying out the operation 
+// post inc = +1 after carrying out the operation
+
+
+// var c = 5
+// var d = 2
+
+// var eq =  d++ + ++d + d++ + c++ + d++ + ++c + c++ + ++c + d++ + c++
+
+// my ans = 58
+//  actual ans = 58  
+ 
+
+// document.writeln(eq)
+
+
+// var a = 0
+// var b = 2
+// var c = 4
+
+// var eq = b++ + ++c + a++ + ++a + ++a + a++ + b++ + b + c++ + c++ + ++a + ++c + b++ + a + b 
+
+// // my ans = 61
+//  actual ans = 61  
+
+
+// document.writeln(eq)
+
+
+
+// var a = 24
+// var b = 12
+// var c = 15
+
+// var eq = a++ + ++b + c++ + ++a + b++ + ++c + a++ + ++b + c++ + ++a + b++ + c++ + a++ + b++ + c++
+
+// my ans = 289
+// actual ans = 290
+
+
+// document.writeln(eq)
+
+var firstname = "Muhammad"
+var middlename = "Anas"
+var lastname = "Raza"
+var age = 19
+var gender = "male"
+var city = "karachi"
+var country = "Pakistan"
+var profession = "student"   
+var DOB = "04-09-2006"
+
+
+document.writeln(
+    "Hello good to meet you, my name is " + firstname + " " + middlename + " " + lastname +
+     +
+    "I am " + age + " years old, and I am a " + gender + "." +
+     +
+    "I live in " + city + ", " + country + "." +
+     +
+    "My profession is " + profession + ", and my date of birth is " + DOB + "."
+)
+
+
+
+// var fullname = firstname + " " + middlename + " " + lastname
+// document.writeln(fullname)
